7fcd70289b40ddf5a4dd52fdda104b4a9ed92084
Yuki Naka <yukinaka2168@au.com>
2024-10-22 10:44:44 +0900

    「ConfirmFragment」内でViewBindingの設定。

--- app/src/main/java/com/example/piano_cover_book/ConfirmFragment.kt
+++ app/src/main/java/com/example/piano_cover_book/ConfirmFragment.kt
@@ -5,17 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.piano_cover_book.databinding.FragmentConfirmBinding
 
 class ConfirmFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentConfirmBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_confirm, container, false)
+    ): View {
+        _binding = FragmentConfirmBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
