567e4056a9c0c363f8b60b2ee567ea7dc9e9f132
Yuki Naka <yukinaka2168@au.com>
2024-10-22 09:21:12 +0900

    「MainActivity」と「activity_main.xml」のコードを修正。

--- app/src/main/java/com/example/piano_cover_book/MainActivity.kt
+++ app/src/main/java/com/example/piano_cover_book/MainActivity.kt
@@ -1,46 +1,20 @@
 package com.example.piano_cover_book
 
 import android.os.Bundle
-import android.widget.SeekBar
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rightNumberTextView = findViewById<TextView>(R.id.right_number_text_view)
-        val rightNumberSeekbar = findViewById<SeekBar>(R.id.right_number_seekbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
-        val leftNumberTextView = findViewById<TextView>(R.id.left_number_text_view)
-        val leftNumberSeekbar = findViewById<SeekBar>(R.id.left_number_seekbar)
-
-        rightNumberTextView.text = "${rightNumberSeekbar.progress}"
-        leftNumberTextView.text = "${leftNumberSeekbar.progress}"
-
-        rightNumberSeekbar.setOnSeekBarChangeListener(
-            object : SeekBar.OnSeekBarChangeListener {
-                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                    rightNumberTextView.text = "$p1"
-                }
-                override fun onStartTrackingTouch(p0: SeekBar?) {
-                }
-                override fun onStopTrackingTouch(p0: SeekBar?) {
-                }
-            }
-        )
-
-        leftNumberSeekbar.setOnSeekBarChangeListener(
-            object : SeekBar.OnSeekBarChangeListener {
-                override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
-                    leftNumberTextView.text = "$p1"
-                }
-                override fun onStartTrackingTouch(p0: SeekBar?) {
-                }
-                override fun onStopTrackingTouch(p0: SeekBar?) {
-                }
-            }
-        )
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
