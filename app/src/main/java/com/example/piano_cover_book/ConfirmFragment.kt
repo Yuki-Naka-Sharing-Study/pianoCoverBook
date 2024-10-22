1cd10462d3cda10d7cf0669ca911c5d5e5493f37
Yuki Naka <yukinaka2168@au.com>
2024-10-22 11:33:38 +0900

    「ConfirmFragment」内でデータがある場合とない場合の条件分岐を記述。「fragment_confirm.xml」内にデータがある場合の画面とない場合の画面のコードを記述。

--- app/src/main/java/com/example/piano_cover_book/ConfirmFragment.kt
+++ app/src/main/java/com/example/piano_cover_book/ConfirmFragment.kt
@@ -18,4 +18,31 @@ class ConfirmFragment : Fragment() {
         _binding = FragmentConfirmBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val data = listOf<String>()
+
+        if (data.isEmpty()) {
+            binding.searchView.visibility = View.GONE
+            binding.searchImageView.visibility = View.GONE
+            binding.searchEditText.visibility = View.GONE
+            binding.musicInfoRecyclerView.visibility = View.GONE
+
+            binding.musicNoteImageView.visibility = View.VISIBLE
+            binding.noRecordTextView.visibility = View.VISIBLE
+            binding.noRecordDescTextView.visibility = View.VISIBLE
+        } else {
+            binding.musicNoteImageView.visibility = View.GONE
+            binding.noRecordTextView.visibility = View.GONE
+            binding.noRecordDescTextView.visibility = View.GONE
+
+            binding.searchView.visibility = View.VISIBLE
+            binding.searchImageView.visibility = View.VISIBLE
+            binding.searchEditText.visibility = View.VISIBLE
+            binding.musicInfoRecyclerView.visibility = View.VISIBLE
+
+        }
+    }
 }
