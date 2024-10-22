66c1a6a447131f4d46319ab6903143336b06c74a
Yuki Naka <yukinaka2168@au.com>
2024-10-22 09:55:30 +0900

    「fragment_record.xml」の作成。「RecordFragment」内でViewBindingとNavigationの設定。

--- app/src/main/java/com/example/piano_cover_book/RecordFragment.kt
+++ app/src/main/java/com/example/piano_cover_book/RecordFragment.kt
@@ -5,17 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.piano_cover_book.databinding.FragmentRecordBinding
 
 class RecordFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentRecordBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_record, container, false)
+    ): View {
+        _binding = FragmentRecordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+
+        binding.nameOfArtistEditText.setOnEditorActionListener { _, _, _ ->
+            // val inputText = binding.editText.text.toString()
+            // 入力されたテキストを処理
+            // println("入力されたテキスト: $inputText")
+            true
+        }
+
+        binding.nameOfSongEditText.setOnEditorActionListener { _, _, _ ->
+            // val inputText = binding.editText.text.toString()
+            // 入力されたテキストを処理
+            // println("入力されたテキスト: $inputText")
+            true
+        }
+
+        binding.memoEditText.setOnEditorActionListener { _, _, _ ->
+            // val inputText = binding.editText.text.toString()
+            // 入力されたテキストを処理
+            // println("入力されたテキスト: $inputText")
+            true
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
